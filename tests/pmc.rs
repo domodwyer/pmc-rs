d9d0b550ed743d21ae024a52e963dde0b940e1bf
Dom <dom@itsallbroken.com>
2021-02-06 13:20:48 +0000

    ci: fix Cirrus CI pipeline
    Disables execution on FreeBSD 12 & 13 pending support for the updated
    libpmc interface (#7).
    
    Cirrus CI uses Google Cloud to run their FreeBSD VMs [1], and while I
    cannot find any official statement regarding PMC support within GCP
    there are multiple reports of Linux's "perf" tool not collecting counter
    values on GCP VMs [2], so it seems likely their hypervisor does not
    expose them.
    
    To have a usable CI pipeline, this change amends the tests to use
    software counters rather than real CPU PMCs. While this means the tests
    are not a complete e2e including hardware support, it does at least
    exercise & validate the integration with libpmc.
    
    [1]: https://cirrus-ci.org/guide/FreeBSD/
    [2]: https://stackoverflow.com/questions/40180636/linux-perf-events-profiling-in-google-compute-engine-not-working

--- tests/pmc.rs
+++ tests/pmc.rs
@@ -4,71 +4,71 @@ use pmc::error::*;
 
 #[test]
 fn test_process_counter() {
-	let mut counter = pmc::Counter::new("unhalted-cycles", &pmc::Scope::Process, pmc::CPU_ANY)
-		.expect("failed to create counter");
+    let mut counter = pmc::Counter::new("PAGE_FAULT.ALL", &pmc::Scope::Process, pmc::CPU_ANY)
+        .expect("failed to create counter");
 
-	read_counter(&mut counter);
+    read_counter(&mut counter);
 }
 
 #[test]
 #[ignore]
 fn test_system_counter() {
-	let mut counter =
-		pmc::Counter::new("cycles", &pmc::Scope::System, 0).expect("failed to create counter");
+    let mut counter =
+        pmc::Counter::new("cycles", &pmc::Scope::System, 0).expect("failed to create counter");
 
-	read_counter(&mut counter);
+    read_counter(&mut counter);
 }
 
 #[test]
 fn test_set_counter() {
-	let mut counter = pmc::Counter::new("LOCK.FAILED", &pmc::Scope::Process, pmc::CPU_ANY)
-		.expect("failed to create counter");
+    let mut counter = pmc::Counter::new("LOCK.FAILED", &pmc::Scope::Process, pmc::CPU_ANY)
+        .expect("failed to create counter");
 
-	counter.set(42).expect("failed to set counter");
-	assert_eq!(counter.read().unwrap(), 42);
-	assert_eq!(counter.set(4242).unwrap(), 42);
+    counter.set(42).expect("failed to set counter");
+    assert_eq!(counter.read().unwrap(), 42);
+    assert_eq!(counter.set(4242).unwrap(), 42);
 }
 
 #[test]
 fn test_counter_bad_name() {
-	assert_eq!(
-		pmc::Counter::new("wat", &pmc::Scope::Process, pmc::CPU_ANY)
-			.unwrap_err()
-			.kind(),
-		&ErrorKind::AllocInit
-	);
+    assert_eq!(
+        pmc::Counter::new("wat", &pmc::Scope::Process, pmc::CPU_ANY)
+            .unwrap_err()
+            .kind(),
+        &ErrorKind::AllocInit
+    );
 }
 
 #[test]
 fn test_null_in_counter_name() {
-	assert_eq!(
-		pmc::Counter::new("instru\0ctions", &pmc::Scope::Process, pmc::CPU_ANY)
-			.unwrap_err()
-			.kind(),
-		&ErrorKind::InvalidEventSpec
-	);
+    assert_eq!(
+        pmc::Counter::new("instru\0ctions", &pmc::Scope::Process, pmc::CPU_ANY)
+            .unwrap_err()
+            .kind(),
+        &ErrorKind::InvalidEventSpec
+    );
 }
 
 #[test]
 fn test_attach_to_pid() {
-	let mut counter = pmc::Counter::new("instructions", &pmc::Scope::Process, pmc::CPU_ANY)
-		.expect("failed to create counter");
+    let mut counter = pmc::Counter::new("instructions", &pmc::Scope::Process, pmc::CPU_ANY)
+        .expect("failed to create counter");
 
-	// pmc_attach treats 0 as "attach to self"
-	counter.attach(0).expect("failed to attach to self");
+    // pmc_attach treats 0 as "attach to self"
+    counter.attach(0).expect("failed to attach to self");
 
-	read_counter(&mut counter);
+    read_counter(&mut counter);
 }
 
 fn read_counter(c: &mut pmc::Counter) {
-	c.start().expect("failed to start counter");
+    c.start().expect("failed to start counter");
 
-	let mut last: u64 = 0;
-	for _ in 1..100 {
-		let now = c.read().expect("unable to read counter");
-		if now < last {
-			panic!("counter decremented")
-		}
-		last = now;
-	}
+    let mut last: u64 = 0;
+    for _ in 1..100 {
+        let now = c.read().expect("unable to read counter");
+        if now < last {
+            panic!("counter decremented")
+        }
+        last = now;
+    }
 }
